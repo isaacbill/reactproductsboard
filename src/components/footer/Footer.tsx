079ba2d5a95acb058797a90316535fa1e1cc27d3
isaac bill <isaacbill247@gmail,com>
2023-07-25 10:39:06 +0300

    react frontend

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,12 @@
+import "./footer.scss";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <span>MarkAdmin</span>
+      <span>© Admin Dashboard</span>
+    </div>
+  );
+};
+
+export default Footer;
